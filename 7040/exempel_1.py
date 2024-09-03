ea7b0c452d83a098d3bd6b2967fc0546de246d25
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-09-03 16:17:53 +0200

    ny

---
+++ 7040/exempel_1.py
@@ -0,0 +1,4 @@
+namn = input("Vad heter du?")
+print("Hej", namn, "!")
+färg = input("Vad är din favoritfärg?")
+print(namn, "din favoritfärg är", färg)
---
+++ 7040/uppgift_2.py
@@ -0,0 +1,4 @@
+namn = input("Hej, vad heter du? ")
+print(namn,"är ett fint namn. ")
+mat = input(namn + " vilken mat gillar du? ")
+print(namn, mat, "är verkligen gott. ")
