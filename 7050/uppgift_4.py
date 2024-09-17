d5dcfac13fd653b7175217ebcf295b9e1ee86bd3
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-09-17 22:17:37 +0200

    nytt

--- 7050/uppgift_4.py
+++ 7050/uppgift_4.py
@@ -1,13 +1,13 @@
 text = input("Ange ett tal: ")
 tal = float(text)
 if tal >= 1 and tal <= 9:
-    print('Talet är ensiffrigt')
+    print('Talet är ett ental')
 elif tal >= 10 and tal <= 99: 
-    print('Talet är tvåsiffrigt')
+    print('Talet är ett tiotal')
 elif tal >= 100 and tal <= 999:
-    print('Talet är tresiffrigt')
+    print('Talet är ett hundratal')
 elif tal >= 1000:
-    print('Talet är minst fyrsiffrigt')
+    print('Talet är minst ett tusental')
 elif tal < 0:
     print('Talet är negativt')
 print('klart')
