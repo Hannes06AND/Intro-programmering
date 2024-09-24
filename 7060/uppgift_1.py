b84a0eeefd18e2166614e42f3bafc470a2430985
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-09-24 16:05:51 +0200

    nytt

---
+++ 7060/uppgift_1.py
@@ -0,0 +1,29 @@
+A = 3
+while A <= 10:
+    print(A)
+    A = A +1
+print('klar')
+
+B = 2
+while B <= 12:
+    print(B)
+    B = B + 2
+print('klar')
+
+C = 10
+while C >= 0:
+    print(C)
+    C = C - 1
+print('klar')
+
+D = 11
+while D >= 2:
+    print(D)
+    D = D - 3
+print('klar')
+
+E = 1
+while E <= 5:
+    print(E*E)
+    E = E + 1
+print('klar')
