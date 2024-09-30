bb26ce088c093cb172898b24ffa99426cc87520b
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-09-30 12:26:42 +0200

    nytt

--- 7060/uppgift_2.py
+++ 7060/uppgift_2.py
@@ -1,1 +1,9 @@
 text1 = input("Hur många heltal vill du ha? ")
+antal = int(text1)
+text2 = input("Vilket är det minsta talet i serien? ")
+minsta = int(text2)
+stop = minsta + antal
+while minsta < stop:
+    print(minsta)
+    minsta = minsta + 1
+print('klar')
