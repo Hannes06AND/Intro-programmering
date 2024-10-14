e2cb7e5afc88537c8271c5d046488c8698079c91
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-10-14 10:25:04 +0200

    nytt

--- 7070/uppgift_1a.py
+++ 7070/uppgift_1a.py
@@ -1,11 +1,13 @@
 import random
 fraga = input("Vill du spela? j/n: ")
-tarning1 = random.randrange(1, 6)
-tarning2 = random.randrange(1, 6)
+tarning1 = random.randint(1, 6)
+tarning2 = random.randint(1, 6)
 if fraga == "j":
     print(tarning1)
     print(tarning2)
 if tarning1 == tarning2:
     print('vinst')
-else: 
+elif tarning1 != tarning2: 
     print('förlust')
+if fraga == "n":
+    print('Vad roligt att du spelade en stund!')
