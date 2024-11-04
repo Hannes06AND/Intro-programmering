8efdc1a3ebf28cfa280006fe21a74ce33cd2349d
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-11-04 13:36:09 +0100

    nytt

--- 7061/uppgift_2.py
+++
@@ -1,8 +0,0 @@
-svar = input("Gissa mitt tal: ")
-tal = int(svar)
-while svar > "42":
-    print('för litet ')
-    svar = input("Gissa mitt tal: ")
-while svar < "42":
-    print('för stort')
-print('Du gissade rätt ')
