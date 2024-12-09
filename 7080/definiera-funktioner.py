fd6e767ff125b0e97520ee337964ac3deb0fa710
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2024-12-09 13:25:40 +0100

    nytt

---
+++ 7080/definiera-funktioner.py
@@ -0,0 +1,12 @@
+def maximum(x, y):
+    if x > y:
+        print(x)
+    else:
+        print(y)
+def addera(x, y):
+    print(x + y)
+def hej(x):
+    print("hej", x)
+maximum(123, 142)
+addera(623, 4123)
+hej("Hannes")
